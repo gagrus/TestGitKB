09cb7a259b8c899308440e85bfbf0183fb532ae0
gagrus <mishapskozlov@gmail.com>
2023-07-29 11:40:32 +0300

    added new NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <div>
+      <ol>
+        <ul>home</ul>
+        <ul>about</ul>
+        <ul>game</ul>
+        <ul>player</ul>
+      </ol>
+    </div>
+  )
+}
